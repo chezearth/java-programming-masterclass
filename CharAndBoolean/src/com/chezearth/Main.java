44fd6dc2c2f8488bb3733ccca77b2cf05f1a39e3
chezearth <rethman@me.com>
2019-07-24 13:33:28 +0200

    Updated work Section 4

---
+++ CharAndBoolean/src/com/chezearth/Main.java
@@ -0,0 +1,18 @@
+package com.chezearth;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = '\u00A9';
+        System.out.println("Unicode copyright symbol is: " + myChar);
+
+        boolean myBool = true;
+        System.out.println("Boolean inverted = " + !myBool);
+
+        char myRegSymbol = '\u00AE';
+        System.out.println("Unicode Registered symbol is: " + myRegSymbol);
+
+    }
+
+}
