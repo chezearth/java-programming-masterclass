12cf23804e12c32a9844b9ba40c5857811afe8df
chezearth <rethman@me.com>
2019-08-27 15:58:43 +0200

    Finished section 3 (at last)

--- operators/src/com/chezearth/Main.java
+++ operators/src/com/chezearth/Main.java
@@ -37,8 +37,36 @@ public class Main {
         System.out.println("result -= 9 = " + result);
 
         boolean isAlien = false;
-        if(!isAlien) System.out.println("It is not an alien!");
+        if (!isAlien) {
+            System.out.println("It is not an alien!");
+        }
 
+        int topScore = 100;
+        if (topScore <= 100) {
+            System.out.println("Highest Score!");
+        }
+
+        int secondHighestScore = 75;
+        if (topScore > secondHighestScore && topScore > 80) {
+            System.out.println("Your score was higher than 80 and the second highest score.");
+        }
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        boolean wasCar = isCar ? true : false;
+        System.out.println("wasCar is " + wasCar);
+
+        double myFirstVar = 20.00d;
+        double mySecondVar = 80.00d;
+
+        boolean answer = ((myFirstVar + mySecondVar) * 100.00d % 40.00d == 0) ? true : false;
+        System.out.println(("Challenge answer is " + answer));
+        if (!answer) {
+            System.out.println("Got some remainder");
+        }
 
     }
 }
