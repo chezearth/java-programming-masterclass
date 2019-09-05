7fee48a7c7275acc9a0a377e0065479cfc8c85aa
chezearth <rethman@me.com>
2019-09-05 15:06:18 +0200

    Renamed projects

---
+++ if-keyword-and-codeblocks/src/com/chezearth/Main.java
@@ -0,0 +1,25 @@
+package com.chezearth;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+}
---
+++ keywords-and-expressions/src/com/chezearth/Main.java
@@ -0,0 +1,14 @@
+package com.chezearth;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // In the following code, write down the parts that are expressions
+        int score = 100; // score = 100
+        if (score > 99) { // score > 99
+            System.out.println("You got the high score!"); // "You got the high score!"
+            score = 0; // score = 0
+        }
+    }
+}
