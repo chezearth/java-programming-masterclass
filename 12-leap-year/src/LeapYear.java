cd1450d14c6fcef7a950a091f369541aff748357
chezearth <rethman@me.com>
2019-09-14 20:21:35 +0200

    Exercise 4 done, 5 started

--- 12-leap-year/src/LeapYear.java
+++ 12-leap-year/src/LeapYear.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class LeapYear {
+
+    public static boolean isLeapYear(int year) {
+        return (year >= 1 && year <= 9999) && (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+    }
+
 }
--- 12-leap-year/src/Main.java
+++ 12-leap-year/src/Main.java
@@ -1,2 +1,19 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("The year 0 is in range? " + LeapYear.isLeapYear(0));
+        System.out.println("The year 1700 is a leap year - " + LeapYear.isLeapYear(1700));
+        System.out.println("The year 1800 is a leap year - " + LeapYear.isLeapYear(1800));
+        System.out.println("The year 1900 is a leap year - " + LeapYear.isLeapYear(1900));
+        System.out.println("The year 2100 is a leap year - " + LeapYear.isLeapYear(2100));
+        System.out.println("The year 2200 is a leap year - " + LeapYear.isLeapYear(2200));
+        System.out.println("The year 2300 is a leap year - " + LeapYear.isLeapYear(2300));
+        System.out.println("The year 2500 is a leap year - " + LeapYear.isLeapYear(2500));
+        System.out.println("The year 2600 is a leap year - " + LeapYear.isLeapYear(2600));
+        System.out.println("The year 1600 is a leap year - " + LeapYear.isLeapYear(1600));
+        System.out.println("The year 1996 is a leap year - " + LeapYear.isLeapYear(1996));
+        System.out.println("The year 2000 is a leap year - " + LeapYear.isLeapYear(2000));
+        System.out.println("The year 2004 is a leap year - " + LeapYear.isLeapYear(2004));
+        System.out.println("The year 2400 is a leap year - " + LeapYear.isLeapYear(2400));
+        System.out.println("The year 10000 is in range - " + LeapYear.isLeapYear(10000));
+    }
 }
