b538d92911e0824af810e7beb49564fba4c6c7bf
chezearth <rethman@me.com>
2019-09-14 15:13:22 +0200

    Practicing first exercise

---
+++ 09-speed-converter/src/SpeedConverter.java
@@ -0,0 +1,14 @@
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        return kilometersPerHour < 0 ? -1 : Math.round(kilometersPerHour / 1.609);
+    }
+
+    public  static void printConversion(double kilometersPerHour) {
+        System.out.println(
+                kilometersPerHour < 0 ?
+                "Invalid Value" :
+                kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h"
+        );
+    }
+}
