f61c3172f010b119d75482a019be388164138816
chezearth <rethman@me.com>
2020-02-02 10:28:08 +0200

    Min array challenge, section 8

--- 43-min-element-challenge/src/com/chezearth/Main.java
+++ 43-min-element-challenge/src/com/chezearth/Main.java
@@ -1,8 +1,43 @@
 package com.chezearth;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+        if (args.length == 0) {
+            System.out.println("Please supply the number of integers required");
+            args = new String[1];
+            args[0] = String.valueOf(readIntegers(1)[0]);
+        }
+        int[] array = readIntegers(Integer.parseInt(args[0]));
+        System.out.println("Min element = " + findMin(array));
+    }
+
+    public static int[] readIntegers(int count) {
+        int[] arr = new int[count];
+        System.out.println("Enter " + count + " integers:\r");
+
+        for (int i = 0; i < count; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        return arr;
+    }
+
+    public static int findMin(int[] arr) {
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+
+        }
+
+        return min;
     }
 }
